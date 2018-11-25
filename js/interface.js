78840c5542972e9b9e4bcb297ef979a4d4f09dae
oio154 <jacek@web.katowice.pl>
2018-11-25 17:35:08 +0100

    20948203984023

---
+++ js/interface.js
@@ -0,0 +1,55 @@
+
+
+
+
+
+
+
+
+
+(
+function(){
+   populateMenu(w);
+}
+)();
+
+function guid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  }
+
+
+
+function populateMenu(words){
+    let folders=[];
+    words.forEach(element => {
+        folders.push( element.folder )
+        
+    });
+    let filterFolders=[...new Set(folders)]; //uniq values
+    console.dir(filterFolders);
+
+    //append list to menu
+
+    filterFolders.forEach(element => {
+        let node=document.createElement("INPUT");
+        let label = document.createElement("LABEL");
+        let id=guid();
+        
+        node.type="checkbox";
+        node.id=id;
+        node.name=element;
+        document.querySelector(".menu").appendChild(node);
+
+        //label
+        label.htmlFor=id;
+        label.appendChild(document.createTextNode(element));
+        document.querySelector(".menu").appendChild(label);
+
+    });
+
+}
---
+++ js/words.js
@@ -0,0 +1,8 @@
+var w=
+[      
+    {pl:"kkkkk", en:"eeeee",folder:"Green 1"}, 
+    {pl:"kkkkk2", en:"eeeee2",folder:"Green 1"}, 
+    {pl:"kkkkk2", en:"eeeee2",folder:"Green 1"},
+    {pl:"kkkkk3-2", en:"eeeee3-2",folder:"Green 3"},
+    {pl:"kkkkk3-2", en:"eeeee3-2",folder:"Green 3"},
+]
